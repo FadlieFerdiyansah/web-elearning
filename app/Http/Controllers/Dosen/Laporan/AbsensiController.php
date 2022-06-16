215e43ef3212eb2782b5592631a855e01198872a
FadlieFerdiyansah <fadlieferdiyansah26@gmail.com>
2022-06-16 07:31:54 +0700

    update

--- app/Http/Controllers/Dosen/Laporan/AbsensiController.php
+++ app/Http/Controllers/Dosen/Laporan/AbsensiController.php
@@ -12,11 +12,12 @@ class AbsensiController extends Controller
     public function __invoke(Request $request)
     {
         info(request('matkul'));
-        $kelas = Auth::user()->matkuls;
-        return $kelas;
-        $mahasiswa = Mahasiswa::with('absens')->whereIn('kelas_id', $kelas->pluck('id'))->orderBy('kelas_id', 'asc')->get();
-        // return $mahasiswa;
-
+        $kelas = Auth::user()->kelas;
+        $mahasiswa = Mahasiswa::with(['absens', 'absens.jadwal' => function($query) use ($kelas) {
+            $query->whereIn('matkul_id', $kelas->pluck('pivot.matkul_id'));
+        }])->whereIn('kelas_id', $kelas->pluck('id'))->orderBy('kelas_id', 'asc')->get();
+        // return $kelas->pluck('pivot.matkul_id');
+        return $mahasiswa;
 
         // $mahasiswa = Mahasiswa::with(['absens' => function($query) {
         //     $query->with(['jadwal' => function($query) {
