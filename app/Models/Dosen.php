fe8959b0baef8c14654b490e1b1d704ec57ef032
Fadlie Ferdiyansah <fadlieferdiyansah26@gmail.com>
2022-07-04 10:22:56 +0700

    fix issue on model

--- app/Models/Dosen.php
+++ app/Models/Dosen.php
@@ -39,7 +39,7 @@ class Dosen extends Authenticatable
 
     public function kelas()
     {
-        return $this->belongsToMany(Kelas::class)->withPivot('matkul_id');
+        return $this->belongsToMany(Kelas::class);
     }
 
     public function matkuls()
