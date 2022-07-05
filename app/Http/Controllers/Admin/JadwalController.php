b4207e848f11efc22b151b0046924d3af3279bb0
FadlieFerdiyansah <fadlieferdiyansah26@gmail.com>
2022-07-05 21:29:55 +0700

    update

--- app/Http/Controllers/Admin/JadwalController.php
+++ app/Http/Controllers/Admin/JadwalController.php
@@ -26,7 +26,7 @@ class JadwalController extends Controller
     {
         Jadwal::create($request->all());
         $dosen = Dosen::where('id', $request->dosen_id)->first();
-        // $dosen->kelas()->updateExistingPivot(['kelas_id' => $request->kelas_id], ['matkul_id' => $request->matkul_id] );
+        $dosen->kelas()->updateExistingPivot(['kelas_id' => $request->kelas_id], ['matkul_id' => $request->matkul_id] );
         $kelas = Kelas::find($request->kelas_id);
         return response()->json(['message' => 'Berhasil membuat jadwal untuk kelas ' . $kelas->kd_kelas]);
     }
--- database/migrations/2021_06_27_151139_create_dosen_kelas_table.php
+++ database/migrations/2021_06_27_151139_create_dosen_kelas_table.php
@@ -16,6 +16,7 @@ class CreateDosenKelasTable extends Migration
         Schema::create('dosen_kelas', function (Blueprint $table) {
             $table->unsignedInteger('dosen_id')->constrained('dosens')->onDelete('cascade');
             $table->unsignedInteger('kelas_id')->constrained('kelas')->onDelete('cascade');
+            $table->unsignedInteger('matkul_id')->nullable();
 
             $table->primary(['dosen_id','kelas_id']);
         });
