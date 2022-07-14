f488ab383f6018f2d421dd1d2b3ffeebbcf1ad8c
FadlieFerdiyansah <fadlieferdiyansah26@gmail.com>
2022-07-14 22:44:17 +0700

    remove unused code

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -4,8 +4,6 @@
             <div class="card">
                 <div class="card-header">
                     <h4>Dashboard Admin</h4>
-                    {{-- <p>{{ Auth::guard('mahasiswa')->user() ? 'ya' : 'no' }}</p>
-                    <p>Role : {{ Auth::guard('mahasiswa')->user()->hasRole('mahasiswa') ? 'mahasiswa' : 'bukan' }}</p> --}}
                 </div>
                 <div class="card-body">
                     <h2>Hi, {{ Auth::user()->nama }}</h2>
